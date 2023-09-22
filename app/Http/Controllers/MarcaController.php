521c23b5dcd97a7a80fe5d5bec9bad843a2d6c7c
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-09-22 04:00:34 -0300

    testing update endpoints

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -54,7 +54,8 @@ class MarcaController extends Controller
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
