d9e9042baaf746f447c2ed79761fe79a152dd3f1
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-09-22 03:20:04 -0300

    defining API routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
