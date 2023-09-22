39b1c1e1361c53f4159b0fb0c7d7c583c2671b26
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-09-22 05:28:41 -0300

    message errors for 'Marca' done

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -35,6 +35,16 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
+        $regras = [
+            'nome' => 'required|unique:marcas',
+            'imagem' => 'required'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca ja existe'
+        ];
+       $request->validate($regras, $feedback);
        $marca = $this->marca->create($request->all());  
        return response()->json($marca. 201);
     
