21832b9ae66e54bab2cece07a17c24e86bbaaaab
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-09-23 01:42:09 -0300

    validating update

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -72,10 +72,30 @@ class MarcaController extends Controller
     {
         $marca = $this->marca->find($id);
 
-        if($marca === null){
-            return response()->json(['erro' => 'Impossivel realizar a atualização. O recurso solicitado não existe'], 404);
+        if($marca === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
         }
-        $request->validate($marca->rules(), $marca->feedback());
+
+        if($request->method() === 'PATCH') {
+
+            $regrasDinamicas = array();
+
+            //percorrendo todas as regras definidas no Model
+            foreach($marca->rules() as $input => $regra) {
+                
+                //coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
+                if(array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            
+            $request->validate($regrasDinamicas, $marca->feedback());
+
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+        
+        
         $marca->update($request->all());
         return response()->json($marca, 200);
     }
