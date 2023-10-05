17832f1936c6622c05c8e0a4c8ac3350533b1843
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-10-05 17:01:58 -0300

    auth web session and API(JWT)

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -5,21 +5,17 @@
      
             <div class="card">
                 <div class="card-header">Login (Component Vue)</div>
-
+                    {{ email}} - {{ password }}
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="" @submit.prevent="login($event)">
                         <input type="hidden" name="_token" :value="csrf_token">
 
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">E-mail</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control" name="email"  required autocomplete="email" autofocus  v-model="email">
 
-                                
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
                      
                             </div>
                         </div>
@@ -28,7 +24,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">Senha</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" v-model="password">
 
                              
                             </div>
@@ -68,7 +64,36 @@
 </template>
 
 <script>
-   export default {
-    props: [ 'csrf_token' ]
+    export default {
+        props: ['csrf_token'], //data (semelhante)
+        data() {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            login(e) {
+
+                let url = 'http://localhost:8000/api/login'
+                let configuracao = {
+                    method: 'post',
+                    body: new URLSearchParams({
+                        'email': this.email,
+                        'password': this.password
+                    })
+                }
+
+                fetch(url, configuracao)
+                    .then(response => response.json())
+                    .then(data => {
+                        if(data.token) {
+                            document.cookie = 'token='+data.token+';SameSite=Lax'
+                        }
+                        //dar sequência no envio do form de autenticação por sessão
+                        e.target.submit()
+                    })
+            }
+        }
     }
 </script>
