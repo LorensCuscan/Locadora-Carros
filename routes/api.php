597d7f771a8efff518d10d0cab382b06c179ab35
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-10-05 13:22:59 -0300

    implementing logout

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::prefix('v1')->middleware('jwt.auth')->group(function(){
     Route::post('me', [ AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::apiResource('cliente', ClienteController::class);
     Route::apiResource('carro', CarroController::class);
@@ -35,6 +36,6 @@ Route::prefix('v1')->middleware('jwt.auth')->group(function(){
 
 
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
+
 
 
