f4d1bf58cd11bebeb87fc5b11bb707b004de05c2
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-10-05 04:58:16 -0300

    defining and testing login with JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,19 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+       
+        $credenciais = $request->all(['email', 'password']);
+
+         //autenticacao (email e senha)
+        $token = auth('api')->attempt($credenciais);
+        if($token){ //usuario autenticado
+            return response()->json(['token' => $token], 200);
+        } else { //usuario nao autenticado ERRO
+            return response()->json(['erro' => 'Usuario ou senha invalido'], 403);
+        }
+        
+        //retornar um token JWT
         return 'login';
     }
     public function logout(){
