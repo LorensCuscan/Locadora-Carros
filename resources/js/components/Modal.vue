82c74880363549deba149dfd2b6ea3ad87838db5
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-10-06 15:22:11 -0300

    fix

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -1,21 +1,22 @@
 <template>
-<div class="modal fade" :id="id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">{{titulo}}</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <button type="button" class="btn btn-primary">Salvar</button>
-      </div>
+    <div class="modal fade" :id="id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">{{titulo}}</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            <div class="modal-body">
+                <slot name="conteudo"></slot>
+            </div>
+                <div class="modal-footer">
+                    <slot name="rodape"></slot>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 </template>
 
 <script>
