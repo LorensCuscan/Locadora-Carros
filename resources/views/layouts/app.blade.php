00cb21b9dd1a408316c87d3f1cff93928557b7e6
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-10-06 13:38:41 -0300

    Adding BreadCrumb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,16 @@
             </div>
         </nav>
 
+        @auth
+        <nav aria-label="breadcrumb">
+            <br>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">{{Route::currentRouteName()}}</li>
+            </ol>
+          </nav>
+        @endauth
+
         <main class="py-4">
             @yield('content')
         </main>
