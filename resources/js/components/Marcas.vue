5f9e5c17e5b5ffe5d46c849f8989f99c7b858328
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-10-06 02:48:42 -0300

    created form for search on 'Marcas'

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -1,20 +1,36 @@
-<template>
-  <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Marcas</div>
 
-                <div class="card-body">
-                <p>Vamos manipular os registros de marca por aqui!</p>
-                           
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Busca de marcas</div>
+
+                    <div class="card-body">
+                     
+                        <div class="row g-3">
+                            <div class="col-md-6 mb-3 d-flex flex-column">
+                                <label for="inputId" class="form-label">ID</label>
+                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                <div id="idHelp" class="form-text text-muted">Opcional. Informe o ID do registro</div>
+                            </div>
+                            <div class="col-md-6 mb-3 d-flex flex-column">
+                                <label for="inputNome" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                                <div id="nomeHelp" class="form-text text-muted">Opcional. Informe o nome da marca</div>
+                            </div>
+                        </div>
+                        <div class="card-footer" >
+                             <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                        </div>
+                    </div>
+                   
+
                 </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
-<script>
-  
-</script>
+
